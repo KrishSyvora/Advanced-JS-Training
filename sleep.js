60ab0ce6d7e7505b0754fed953f636c76f0a84f5
Krish Ojha <kojha@qodeleaf.com>
2025-02-20 19:08:32 +0530

    Removed unnecessary files

--- sleep.js
+++
@@ -1,8 +0,0 @@
-function sleep(seconds) {
-    return new Promise(resolve => {
-        setTimeout(resolve, seconds * 1000);
-    });
-}
-sleep(3).then(() => {
-    console.log("After 3 seconds");
-});
